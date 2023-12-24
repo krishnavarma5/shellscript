fd4890a10d4675de1129513b33344ebfb539fc18
krishnavarma5 <krishnavarma5penmetsa@gmail.com>
2023-12-25 08:53:59 +1030

    updated file new

---
+++ shellscript/07-arrays.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+FRUITS=("Apple" "Banana" "Manga")
+echo "First value: ${FRUITS[0]}"
+
+echo "Second value: ${FRUITS[1]}"
+
+echo "Third value: ${FRUITS[2]}"
+
+echo "All Fruits: ${FRUITS[@]}"
---
+++ shellscript/09-install-mysql.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+if [ $ID -ne 0 ]
+then
+   echo "ERROR :: please run this script with root access"
+   exit 1 # you can give other than 0
+else
+   echo "you are root user"
+fi
+    
+yum install mysql -y
+
+if [ $? -ne 0]
+then 
+   echo "ERROR :: Installing mysql is failed"
+   exit 1
+else
+    echo "Installing MySQL is success"
+fi        
