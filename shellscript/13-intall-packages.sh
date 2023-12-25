e211e710e7c9b045459f7d66d371ef08297a3bd8
krishnavarma5 <krishnavarma5penmetsa@gmail.com>
2023-12-25 10:57:18 +1030

    updated file krishna

--- shellscript/13-intall-packages.sh
+++ shellscript/13-intall-packages.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
 
 ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESSTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESSTAMP.log"
+
+echo "script started executing at $TIMESSTAMP" &>> $LOGFILE
+
+VALIDATE(){ 
+   if [$1 -ne o]
+   then 
+    echo -e "$2 ... $R FAILED $N"
+    else 
+    echo -e "$2 ... $G SUCCESS $N"
+    fi
+   }
 
 if [ $ID -ne 0 ]
 then
@@ -10,4 +28,18 @@ else
    echo "you are root user"
 fi #fi means reverse of if, indicating condition end
 
-echo "All arguments passed: $@"
+#echo "All arguments passed: $@"
+# git mysql postfix net-tools
+
+for package in $@ 
+do
+  yum list installed $package &>> $LOGFILE #check installed or not 
+  if [ $? -ne 0] #if not installed
+   then 
+      yum install $package -y &>> $LOGFILE # install the package
+      VALIDATE $? "installation of $package" # validate
+   else
+      ECHO -E "$package is alreaddy installed ... $Y SKIPPING $N"
+      fi
+   done
+
