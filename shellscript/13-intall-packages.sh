a9da99af448ae90fe973c33cb420eab98645b3aa
krishnavarma5 <krishnavarma5penmetsa@gmail.com>
2023-12-25 15:01:29 +1030

    updated file krishna

--- shellscript/13-intall-packages.sh
+++ shellscript/13-intall-packages.sh
@@ -35,7 +35,7 @@ fi #fi means reverse of if, indicating condition end
 for package in $@ 
 do
      yum list installed $package &>> $LOGFILE #check installed or not 
-     if [ $? -ne 0] #if not installed
+     if [ $? -ne 0 ] #if not installed
       then 
          yum install $package -y &>> $LOGFILE # install the package
          VALIDATE $? "installation of $package" # validate
