4da9bf58fd3ed6527a293d66353c82ead65231d4
krishnavarma5 <krishnavarma5penmetsa@gmail.com>
2023-12-29 19:38:46 +1030

    updated file

--- shellscript/17-disk-usage.sh
+++ shellscript/17-disk-usage.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-DISK_USAGE=$($ df -hT | grep -vE 'tmp|File')
+DISK_USAGE=$(df -hT | grep -vE 'tmp|File')
 DISK_THRESHOLD=1
 message=""
 
 while IFS= read line
 do
    usage=$(echo $line | awk '{print $6F}' | Cut -d % -f1)
-   partition=$(echo $line | awk '{print $1F}'
+   partition=$(echo $line | awk '{print $1F}')
    if [ $usage -ge $DISK_THRESHOLD ]
    then 
       message="High Disk usage on $partition: $usage"
     fi  
 done <<< $Disk_USAGE
 
-echo "message: $message"
+echo "Message: $message"
