684a19d1cfd75b47fddaefaaaa71ed81c08128cf
krishnavarma5 <krishnavarma5penmetsa@gmail.com>
2023-12-30 12:26:56 +1030

    updated file

--- shellscript/17-disk-usage.sh
+++ shellscript/17-disk-usage.sh
@@ -16,4 +16,6 @@ done <<< $DISK_USAGE
 
 echo -e "Message: $message"
 
-echo "$message" | mail -s "High Disk Usage" krishnapenmetsa05@gmail.com
+#echo "$message" | mail -s "High Disk Usage" krishnapenmetsa05@gmail.com
+
+sh mail.sh "DevOps Team" "High Disk Usage" "$message" "krishnapenmetsa05gmail.com" "Alert High Disk Usage"
