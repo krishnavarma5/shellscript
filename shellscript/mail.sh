8ce585ec6569307a4bb4db78393f1ec86b22313f
krishnavarma5 <krishnavarma5penmetsa@gmail.com>
2023-12-31 09:22:01 +1030

    updated file

--- shellscript/mail.sh
+++ shellscript/mail.sh
@@ -7,6 +7,6 @@ ESCAPE_BODY=$(printf '%s\n' "$BODY" | sed -e 's/[]\/$*.^[]/\\&/g');
 TO_ADDRESS=$4
 SUBJECT=$5
 
-FINAL_BODY=$(sed -e "s/To_Team/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/BODY/$ESCAPE_BODY/g" template.html)
+FINAL_BODY=$(sed -e "s/To_Team/$TO_TEAM/g" -e "s/Alert_Type/$ALERT_TYPE/g" -e "s/BODY/$ESCAPE_BODY/g" template.html)
 
 echo "$FINAL_BODY" | mail -s "$(echo -e "$SUBJECT\nContent-Type: text/html")" "$TO_ADDRESS"
